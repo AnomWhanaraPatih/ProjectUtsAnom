690afc26138db92788d687d665b17430aaea4024
Anom Whanara Patih <2110031802071@sar.ac.id>
2023-05-17 23:15:55 -0800

    Initial commit

---
+++ app/src/main/java/com/example/projectutsanom/fragment/ExploreFragment.kt
@@ -0,0 +1,114 @@
+package com.example.projectutsanom.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projectutsanom.DetailMovieActivity
+import com.example.projectutsanom.Movie
+import com.example.projectutsanom.MovieAdapter
+import com.example.projectutsanom.R
+
+class ExploreFragment : Fragment() {
+
+    companion object {
+        val INTENT_PARCELABLE = "OBJECT_INTENT"
+    }
+
+    private lateinit var adapter : MovieAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var movieArrayList : ArrayList<Movie>
+
+    lateinit var image: Array<Int>
+    lateinit var title: Array<String>
+    lateinit var descriptions: Array<String>
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_explore, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dataInitialize()
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView = view.findViewById(R.id.rv_movie)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+
+        recyclerView.adapter = context?.let {
+            MovieAdapter(it, movieArrayList){
+                val intent = Intent(context, DetailMovieActivity::class.java)
+                intent.putExtra(INTENT_PARCELABLE, it)
+                startActivity(intent)
+            }
+        }
+
+    }
+
+    private fun dataInitialize() {
+        movieArrayList = arrayListOf<Movie>()
+
+        image = arrayOf(
+            R.drawable.js,
+            R.drawable.python,
+            R.drawable.css,
+            R.drawable.cplus,
+            R.drawable.java,
+            R.drawable.kotlin,
+            R.drawable.ruby,
+            R.drawable.cstring,
+            R.drawable.php,
+            R.drawable.html,
+
+            )
+
+        title = arrayOf(
+            "Javascript",
+            "Python",
+            "CSS",
+            "C++",
+            "Java",
+            "Kotlin",
+            "Ruby",
+            "C#",
+            "PHP",
+            "HTML",
+
+
+            )
+
+        descriptions = arrayOf(
+            "JavaScript adalah suatu bahasa pemrograman tingkat tinggi dan dinamis. JavaScript populer di internet dan dapat bekerja di sebagian besar penjelajah web populer seperti Google Chrome, Internet Explorer, Mozilla Firefox, Netscape dan Opera. Kode JavaScript dapat disisipkan dalam halaman web menggunakan tag script",
+            "Python adalah bahasa pemrograman tujuan umum yang ditafsirkan, tingkat tinggi. Dibuat oleh Guido van Rossum dan pertama kali dirilis pada tahun 1991, filosofi desain Python menekankan keterbacaan kode dengan penggunaan spasi putih yang signifikan.",
+            "Cascading Style Sheet merupakan aturan untuk mengatur beberapa komponen dalam sebuah web sehingga akan lebih terstruktur dan seragam. CSS bukan merupakan bahasa pemrograman.",
+            "C++ adalah bahasa pemrograman komputer yang dibuat oleh Bjarne Stroustrup, yang merupakan perkembangan dari bahasa C dikembangkan di Bell Labs. Pada awal tahun 1970-an, bahasa itu merupakan peningkatan dari bahasa sebelumnya, yaitu B.",
+            "Java adalah bahasa pemrograman yang dapat dijalankan di berbagai komputer termasuk telepon genggam. Bahasa ini awalnya dibuat oleh James Gosling saat masih bergabung di Sun Microsystems, yang saat ini merupakan bagian dari Oracle dan dirilis tahun 1995.",
+            "Kotlin adalah sebuah bahasa pemrograman dengan pengetikan statis yang berjalan pada Mesin Virtual Java ataupun menggunakan kompiler LLVM yang dapat pula dikompilasikan kedalam bentuk kode sumber JavaScript. Pengembang utamanya berasal dari tim programer dari JetBrains yang bermarkas di Rusia.",
+            "Ruby adalah bahasa pemrograman dinamis berbasis skrip yang berorientasi objek. Tujuan dari ruby adalah menggabungkan kelebihan dari semua bahasa-bahasa pemrograman skrip yang ada di dunia. Ruby ditulis dengan bahasa pemrograman C dengan kemampuan dasar seperti Perl dan Python.",
+            "C# adalah sebuah bahasa pemrograman berorientasi objek yang dikembangkan oleh Microsoft sebagai bagian dari inisiatif kerangka .NET Framework.",
+            "PHP: Hypertext Preprocessor atau hanya PHP saja, adalah bahasa skrip dengan fungsi umum yang terutama digunakan untuk pengembangan web. Bahasa ini awalnya dibuat oleh seorang pemrogram Denmark-Kanada Rasmus Lerdorf pada tahun 1994. Implementasi referensi PHP sekarang diproduksi oleh The PHP Group.",
+            "Hypertext Markup Language adalah bahasa markah standar untuk dokumen yang dirancang untuk ditampilkan di peramban internet. Ini dapat dibantu oleh teknologi seperti Cascading Style Sheets dan bahasa skrip lainnya seperti JavaScript, VBScript, dan PHP.",
+
+
+            )
+
+        for (i in image.indices) {
+
+            val movie = Movie(image[i],title[i], descriptions[i])
+            movieArrayList.add(movie)
+        }
+
+    }
+
+
+}
